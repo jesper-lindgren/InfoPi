f16b844c4b698827cab20f6f109b92adc0acf786
Jesper Lindgren <jespereriklindgren@gmail.com>
2017-01-08 20:49:31 +0100

    Changed gpio handler to output a json object on event

--- python/gpio_handler.py
+++ python/gpio_handler.py
@@ -1,5 +1,6 @@
 import RPi.GPIO as GPIO
 import time
+import json
 
 btns_map = [26]
 btns_times = [0]
@@ -7,7 +8,7 @@ btns_state = [1]
 
 def btnEventCB(channel):
 	index = btns_map.index(channel)
-	print('Event! on pin ' + str(channel) + ' with index ' + str(index))
+	# print('Event! on pin ' + str(channel) + ' with index ' + str(index))
 	# If the previous state was HIGH
 	if btns_state[index] == 1:
 		# Store the start time
@@ -15,13 +16,16 @@ def btnEventCB(channel):
 	else:
 		# Check how long the btn was pressed
 		pressed = time.time() - btns_times[index]
+		eventObj = {'eventType': 'buttonPressed', 'subtype': '', 'buttonIndex': index}
 		if pressed > 2:
+			eventObj['subtype'] = 'longPress'
 			print('Long press (> 2 s)')
 		else:
+			eventObj['subtype'] = 'shortPress'
 			print('Short press (< 2 s)')
+		print(json.dumps(eventObj))
 	# Store the new btn state
 	btns_state[btns_map.index(channel)] = GPIO.input(channel)
-
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(26, GPIO.IN, pull_up_down=GPIO.PUD_UP)
