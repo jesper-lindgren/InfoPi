86d7ae8de44df8fa5fcfec77fe95d560574fb714
Jesper Lindgren <jespereriklindgren@gmail.com>
2017-01-08 11:44:23 +0100

    Working sensor readout

--- infopi-server/app.js
+++ infopi-server/app.js
@@ -11,6 +11,7 @@ var users = require('./routes/users');
 // User requires
 var Tail = require('tail').Tail;
 var noble = require('noble');
+var _ = require('underscore');
 
 var app = express();
 
@@ -51,6 +52,9 @@ app.use(function(err, req, res, next) {
 var serviceUuids = ['aa80']; // The CC2650 sensortag
 
 var batteryServiceUUID = '180f'; // Battery service uuid
+var humiditySensorUUID = 'f000aa2004514000b000000000000000' // Humidity sensor uuid
+// var humiditySensorUUID = 'F000AA2004514000B000000000000000';
+var CC2560serviceUUIDs = [batteryServiceUUID, humiditySensorUUID]
 var batteryServiceValueCharacteristicsUuid = '2a19'; // The value char uuid
 
 var allowDuplicates = false;
@@ -89,22 +93,91 @@ var requestNotify = function(characteristic) {
 
 var ble_setup_service = function(err, services) {
   if (err) throw err;
+
+  // Log the available services
+  _.each(services, function(s) {
+      console.log(new Date() + ' ' + 'Found service with uuid: ' + s.uuid);
+  });
+
   services.forEach(function(service) {
     if (service.uuid === batteryServiceUUID) {
       console.log(new Date() + ' ' + 'Found a battery service UUID');
       var characteristicsUUIDs = [batteryServiceValueCharacteristicsUuid];
       service.discoverCharacteristics(characteristicsUUIDs, function(err, characteristics) {
-        console.log(new Date() + ' ' + 'Got characteristics');
-        requestNotify(characteristics[0]); // This is the battery level char
+        console.log(new Date() + ' ' + 'Got characteristics for battery service');
+        requestNotify(characteristics[0]); // This is the battery level char (only one present)
       });
     }
+
+    if (service.uuid === humiditySensorUUID) {
+      var DATA_UUID = 'f000aa2104514000b000000000000000';
+      var CONFIG_UUID = 'f000aa2204514000b000000000000000';
+      var PERIOD_UUID = 'f000aa2304514000b000000000000000';
+
+      console.log(new Date() + ' ' + 'Found a humidity service UUID');
+      var characteristicsUUIDs = [DATA_UUID, CONFIG_UUID, PERIOD_UUID];
+      service.discoverCharacteristics([], function(err, characteristics) {
+        console.log(new Date() + ' ' + 'Got characteristics for humidity sensor');
+
+        var dataChar = null;
+        var configChar = null;
+        var periodChar = null;
+
+        _.each(characteristics, function(c) {
+          switch (c.uuid) {
+            case DATA_UUID:
+              dataChar = c;
+              break;
+            case CONFIG_UUID:
+              configChar = c;
+              break;
+            case PERIOD_UUID:
+              periodChar = c;
+              break;
+            default:
+              console.log(new Date() + ' ' + _.pick(c, ['uuid', 'name', 'type', 'properties', 'descriptors']));
+              break;
+          }
+        });
+
+        // Only proceed if we found all chars
+        if (dataChar && configChar && periodChar) {
+          
+          configChar.write(new Buffer([0x01]), true, function(err) {
+            console.log(new Date() + ' ' + 'Enabled humidity sensor with ' + (err ? 'error' : 'no error'));
+          });
+
+          dataChar.on('notify', function(state) {
+            console.log(new Date() + ' ' + 'Humidity sensor dataChar notify is : ' + (state ? 'on' : 'off'));
+          });
+
+          dataChar.on('read', function(data, isNotification) {
+            var dataVal = (data[1] << 8) + data[0];
+            var dataCalc = (dataVal / 65536) * 165 - 40;
+            
+            var humidityVal = (data[3] << 8) + data[2];
+            var humidityCalc = (humidityVal / 65536) * 100;
+
+            // console.log(dataVal, dataCalc, humidityVal, humidityCalc);
+            console.log(new Date() + ' ' + 'New humidity sensor data, temp: ' + dataCalc.toFixed(1) + '°C, humidity: ' + humidityCalc.toFixed(1) + '%');
+          });
+
+          dataChar.notify(true, function(err) {
+            if (err) console.log(new Date() + ' ' + 'Enabled notifications for humudity sensor data char with ' + (err ? 'error' : 'no error'));
+
+          })
+
+        }
+      });
+    }
+
   });
 }
 
 var ble_connect = function(err) {
   if (err) throw err;
 
-  // Workaround: Continue scanning
+  // Continue scanning once connected
   noble.startScanning(serviceUuids, allowDuplicates);
 
   console.log(new Date() + ' ' + 'Connection to ' + this.peripheral.uuid);
@@ -120,17 +193,27 @@ var ble_connect = function(err) {
     process.on('SIGINT', exitHandler);
   }
 
+  // this.peripheral.discoverServices(CC2560serviceUUIDs, ble_setup_service);
   this.peripheral.discoverServices([], ble_setup_service);
 }
 
 var ble_discover = function(peripheral) {
-  console.log(new Date() + ' ' + '(scan) found peripheral: ' + peripheral.advertisement.localName + ' - UUID: ' + peripheral.uuid);
-  deviceMapping[peripheral.uuid] = peripheral.advertisement.localName;
-  peripheral.connect(ble_connect.bind({peripheral: peripheral}));
+  // Stop scanning while connecting
+  noble.stopScanning();
 
-  peripheral.on('disconnect', function(err) {
-    console.log(new Date() + ' '+ 'Disconnected from ' + peripheral.uuid);
-  });
+  console.log(new Date() + ' ' + '(scan) found peripheral: ' + peripheral.advertisement.localName + ' - UUID: ' + peripheral.uuid);
+  // peripheral_print(peripheral);
+
+  // Only connect to the p if not already connected
+  if (deviceMapping[peripheral.uuid] != peripheral.advertisement.localName) {
+
+    deviceMapping[peripheral.uuid] = peripheral.advertisement.localName;
+    peripheral.connect(ble_connect.bind({peripheral: peripheral}));
+
+    peripheral.on('disconnect', function(err) {
+      console.log(new Date() + ' '+ 'Disconnected from ' + peripheral.uuid);
+    });
+  }
 }
 
 // Start scanning
@@ -142,6 +225,20 @@ noble.on('stateChange', function(state) {
 });
 
 noble.on('discover', ble_discover);
+
+var peripheral_print = function(p) {
+  console.log(new Date() + ' ' + 'Peripheral: ');
+  console.log(_.pick(p, ['id', 'address', 'addressType', 'connectable', 'advertisement', 'rssi']));
+  p.discoverAllServicesAndCharacteristics(function(error, services, characteristics) {
+    console.log(services);
+    _.each(services, function(s) {
+      s.discoverCharacteristics(function(err, chars) {
+        console.log(chars);
+      })
+    })
+    // console.log(characteristics);
+  });
+}
 
 
 // noble.on('discover', function(p) {
